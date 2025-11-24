8016583acf3c1c5991523dc00151cbb4f17a0f99
NikStroganov <nikita.stroganov.1999@mail.ru>
2025-11-24 23:58:14 +0300

    Спека для метода отправки email lля восстановления пароля

--- src/main/java/com/auth/email/services/EmailService.java
+++ src/main/java/com/auth/email/services/EmailService.java
@@ -12,9 +12,16 @@ public class EmailService {
         this.mailSender = mailSender;
     }
 
+    /**
+     * Отправляет электронное письмо пользователю для восстановления пароля
+     * @param email - адрес письма получателя
+     * @param subject - тема письма
+     * @param body - тело (текст) письма с временным токеном
+     */
     public void sendEmail (String email, String subject, String body){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(email);
+        //TODO прописать корректный email
         mailMessage.setFrom("Our email");
         mailMessage.setSubject(subject);
         mailMessage.setText(body);
