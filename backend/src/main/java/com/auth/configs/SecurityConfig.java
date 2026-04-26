dcbaa24708a6432fd0a6bb23129acd3981ab5154
Sorokin Andrey <sorokinaa@bcs.ru>
2026-04-26 19:33:02 +0300

    fix cors

--- backend/src/main/java/com/auth/configs/SecurityConfig.java
+++ backend/src/main/java/com/auth/configs/SecurityConfig.java
@@ -65,7 +65,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://careit.tech",
+                "https://www.careit.tech"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
