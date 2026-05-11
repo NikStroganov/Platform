d389fb3a156119310f0e1ef744f721a0f3132b76
Sorokin Andrey <sorokinaa@bcs.ru>
2026-05-11 12:46:35 +0300

    fix storybook

--- frontend/components/shared/icon/icon.tsx
+++ frontend/components/shared/icon/icon.tsx
@@ -2,15 +2,31 @@ import Box, { type BoxProps } from "@mui/material/Box";
 import { type ComponentType, type SVGProps } from "react";
 
 export type SvgIconComponent = ComponentType<SVGProps<SVGSVGElement>>;
+type SvgAssetObject = { src: string };
+type IconComponentProp = SvgIconComponent | string | SvgAssetObject;
 
-export type IconProps = Omit<BoxProps<SvgIconComponent>, "component"> & {
-  component: SvgIconComponent;
+export type IconProps = Omit<BoxProps, "component"> & {
+  component: IconComponentProp;
 };
 
 export function Icon({ component, sx, ...props }: IconProps) {
+  const isSvgComponent = typeof component === "function";
+  const iconSource =
+    typeof component === "string"
+      ? component
+      : typeof component === "object" &&
+          component !== null &&
+          "src" in component &&
+          typeof component.src === "string"
+        ? component.src
+        : undefined;
+
   return (
     <Box
-      component={component}
+      component={isSvgComponent ? component : "img"}
+      src={isSvgComponent ? undefined : iconSource}
+      alt={isSvgComponent ? undefined : ""}
+      aria-hidden={isSvgComponent ? undefined : true}
       sx={[
         {
           width: "1.25rem",
